6e901e5b4e117a85d9b62948e9dceb8f577e0f2c
yogeshGit11 <yogeshGit11@users.noreply.github.com>
2025-11-05 07:18:10 +0000

    deploy: b0839192449aed61a5cb1527f9c8f52f0a4a6ab2

---
+++ _next/static/c2Zj7eXb0HgeSHH5YU8iC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-5fbebdd3a5ca2705.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
